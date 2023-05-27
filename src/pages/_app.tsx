70962cc6694fe1a7828d8900f71a1bab2ed7d148
SahilMahadwar <mahadwarsahil@gmail.com>
2023-05-27 13:06:34 +0530

    feat: :lipstick: add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,34 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { api } from "@/lib/api";
 import "@/styles/globals.css";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style
+        jsx
+        global
+      >{`:root { --font-sans: ${fontSans.style.fontFamily};}}`}</style>
+      <style jsx global>{`:root dark`}</style>
+      <SessionProvider session={session}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </>
   );
 };
 
